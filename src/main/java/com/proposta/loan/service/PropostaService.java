2d7426da7b077ea429a3733c9083e444d9128cd2
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-05-30 16:30:28 -0300

    fix: refactor service to send message rabbitMQ with proposta

--- src/main/java/com/proposta/loan/service/PropostaService.java
+++ src/main/java/com/proposta/loan/service/PropostaService.java
@@ -5,6 +5,7 @@ import com.proposta.loan.dto.PropostaResponseDto;
 import com.proposta.loan.entity.Proposta;
 import com.proposta.loan.mapper.PropostaMapper;
 import com.proposta.loan.repository.PropostaRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,16 +15,34 @@ public class PropostaService {
 
     private PropostaMapper propostaMapper;
     private PropostaRepository propostaRepository;
+    private NotificacaoService notificacaoService;
+    private String exchange;
 
-    public PropostaService(PropostaMapper propostaMapper, PropostaRepository propostaRepository) {
+    public PropostaService(PropostaMapper propostaMapper,
+                           PropostaRepository propostaRepository,
+                           NotificacaoService notificacaoService,
+                           @Value("${rabbitmq.propostapendente.exchange}") String exchange)
+    {
         this.propostaMapper = propostaMapper;
         this.propostaRepository = propostaRepository;
+        this.notificacaoService = notificacaoService;
+        this.exchange = exchange;
     }
 
     public PropostaResponseDto criar(PropostaRequestDto propostaRequestDto){
         Proposta proposta = propostaMapper.convertDtoToProposta(propostaRequestDto);
         propostaRepository.save(proposta);
+        notificarRabbitMQ(proposta);
         return propostaMapper.convertPropostaToDto(proposta);
+    }
+
+    public void notificarRabbitMQ(Proposta proposta) {
+        try {
+            notificacaoService.notificarExchange(proposta, exchange);
+        } catch (RuntimeException e) {
+            proposta.setIntegrada(false);
+            propostaRepository.save(proposta);
+        }
     }
 
     public List<PropostaResponseDto> obterProposta(){
