b99b80a5eccd0fb4efe0a36c4494b6eaab01e4d6
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-05-30 16:28:15 -0300

    feat: add notificacao exchange

---
+++ src/main/java/com/proposta/loan/service/NotificacaoService.java
@@ -0,0 +1,19 @@
+package com.proposta.loan.service;
+
+import com.proposta.loan.entity.Proposta;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificacaoService {
+
+    private RabbitTemplate rabbitTemplate;
+
+    public NotificacaoService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void notificarExchange(Proposta proposta, String exchange){
+        rabbitTemplate.convertAndSend(exchange, "", proposta);
+    }
+}
