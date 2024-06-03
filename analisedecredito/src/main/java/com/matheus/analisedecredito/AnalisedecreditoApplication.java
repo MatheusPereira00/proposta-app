5db0509c2ab6f0aed8e0f655a20346cdb99bf839
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-06-03 10:52:34 -0300

    first analise de credito

---
+++ analisedecredito/src/main/java/com/matheus/analisedecredito/AnalisedecreditoApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.analisedecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisedecreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisedecreditoApplication.class, args);
+	}
+
+}
---
+++ analisedecredito/src/test/java/com/matheus/analisedecredito/AnalisedecreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.analisedecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisedecreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
